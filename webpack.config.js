f0ea7673a81d36e94cf16c62ac98563b467e9ea1
Tlangelani Mboweni <mbowenit@gmail.com>
2017-04-16 22:45:02 +0200

    create src dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './script.js',
+    entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'transpiled.js'
